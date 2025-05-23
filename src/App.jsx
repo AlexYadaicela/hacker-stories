6f5ad400395ab4544737b1b1a2f5a4326f138b53
spycrab <wialex121@gmail.com>
2025-05-23 13:38:43 -0400

    commit file

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react'; 
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  const [title, setTitle] = React.useState('React'); 
+  const welcome = {
+    title: 'React', 
+    greeting: 'Hey'
+  }
+  const nums = [1, 2, 3, 4, 5]; 
+  const liItem = nums.map((num) => 
+    <li>{num}</li>
   )
+
+    return(
+      <div>
+        <h1>Hello <i>{title}</i></h1>
+        <label htmlFor="title">Enter a title</label>
+        <input type="text" name="title" id="title" />
+        <button onClick={() => {
+          const input = document.querySelector('#title'); 
+          setTitle(input.value); 
+        }}>Change Title</button>
+
+        <ul>
+          {liItem}
+        </ul>
+      </div>
+    )
 }
 
-export default App
+export default App; 
